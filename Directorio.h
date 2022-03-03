d72c7634f348908180cc74805501e435519bc0ec
Juan Jose Restrepo <restrepotorojuanjose@gmail.com>
2022-03-03 11:35:11 -0500

    FIX: Fix the previous upload

--- Directorio.h
+++ Directorio.h
@@ -1,9 +1,8 @@
 
 #ifndef DIRECTORIO_H
 #define DIRECTORIO_H
-#include <iostream>
 #include "PropietarioXMascota.h"
-#include <unordered_map>
+#include <map>
 #include <vector>
 
 using namespace std;
@@ -12,16 +11,9 @@ class Directorio{
     private:
         int contadorPropietarios;
         vector <PropietarioXMascota> propietariosYmascotas;
-        unordered_map <int, Propietario> mapaPropietario;
-        unordered_map <int, Mascota> mapaMascota;
-        unordered_map <int, Propietario>::iterator p;
+
     public:
-        void eliminarPropietarioAunaMascota(int identification);
-        void pedirDatosMascota();
-        void agregarMascota(Mascota mascota)
-        void pedirDatosPropietario();
-        void agregarPropietariosxMascota(PropietarioXMascota);
-        void agregarPropietario(Propietario propietario);
+        void agregarPropietarios(PropietarioXMascota);
         void eliminarPropietario(int identification);
         void modificarPropietario(int identification);
         void listarPropietarios(); 
